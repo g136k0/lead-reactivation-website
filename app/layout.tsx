b80f518a92246c705a19c05367a6eda39baf58e4
g136k0 <g136k0@gmail.com>
2026-09-25 03:22:43 +0200

    Add Omnisend site tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,25 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script id="omnisend-tracking" strategy="afterInteractive">
+          {`
+            window.omnisend = window.omnisend || [];
+            window.omnisend.push(["brandID", "6ab5c763f65e46dae6b9acc2"]);
+            window.omnisend.push(["track", "$pageViewed"]);
+
+            (function () {
+              var script = document.createElement("script");
+              script.type = "text/javascript";
+              script.async = true;
+              script.src = "https://omnisnippet1.com/inshop/launcher-v2.js";
+              var firstScript = document.getElementsByTagName("script")[0];
+              firstScript.parentNode.insertBefore(script, firstScript);
+            })();
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
